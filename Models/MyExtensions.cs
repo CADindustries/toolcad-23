43b0dda73b0666d3e37cd517419f4c6f3a283e52
Airat <abdrakovairat@gmail.com>
2023-03-04 18:31:54 +0300

    some randomizing methods added, Vector should be rewritten as a class

---
+++ Models/MyExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toolcad23.Models
+{
+    internal static class MyExtensions
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = list.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public static T Pop<T>(this IList<T> list, int index)
+        {
+            T r = list[index];
+            list.RemoveAt(index);
+            return r;
+        }
+    }
+}
--- ViewModels/RetrievePageViewModel.cs
+++ ViewModels/RetrievePageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -98,11 +99,88 @@ namespace toolcad23.ViewModels
                 return;
             }
 
+            if (parsedMaxGreen > 3 || parsedMaxRed > 3)
+            {
+                MessageBox.Show("Максимально допустимое кол-во кубов в комнате не должно превосходить 3");
+                return;
+            }
+
+            if (parsedMaxGreen < (parsedWhite + parsedBlue) / 4)
+            {
+                MessageBox.Show("Белых и синих кубов больше, чем максимально допустимое");
+                return;
+            }
+            if (parsedMaxRed < parsedYellow / 4)
+            {
+                MessageBox.Show("Жёлтых кубов больше, чем максимально допустимое");
+                return;
+            }
+
             await Task.Run(() =>
             {
+                List<Vector> allowedGreenList = GenerateAllowedList(parsedMaxGreen);
+                List<Vector> allowedRedList = GenerateAllowedList(parsedMaxRed);
 
+                List<string> greenStandElements = GenerateRandomElements(parsedWhite, parsedBlue);
+                List<string> yellowStandElements = Enumerable.Repeat(CubeTypeEnum.Yellow, parsedYellow).ToList();
+
+                Dictionary<string, Vector> greenStandCubes = new Dictionary<string, Vector>();
+                Dictionary<string, Vector> redStandCubes = new Dictionary<string, Vector>();
+
+                while (greenStandElements.Count > 0)
+                {
+                    greenStandCubes.Add(greenStandElements.Pop(0), allowedGreenList.Pop(0));
+                }
+
+                while (yellowStandElements.Count > 0)
+                {
+                    redStandCubes.Add(yellowStandElements.Pop(0), allowedRedList.Pop(0));
+                }
             });
         }
+
+        private void LowDownCubes(Dictionary<string, Vector> standCubes)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                var currentRoomElements = standCubes.Select(x => x.Value).Where(x => x.X == i).ToList();
+                var min = currentRoomElements.Select(x => x.Y).Min();
+                for (int j = 0; j < currentRoomElements.Count; j++)
+                {
+                    // currentRoomElements[i].Y -= 3;
+                }  
+            }
+        }
+
+        private List<string> GenerateRandomElements(int white, int blue)
+        {
+            List<string> elements = new List<string>();
+            for (int i = 0; i < white; i++)
+            {
+                elements.Add(CubeTypeEnum.White);
+            }
+            for (int i = 0; i < blue; i++)
+            {
+                elements.Add(CubeTypeEnum.Blue);
+            }
+            elements.Shuffle();
+            return elements;
+        }
+
+        private List<Vector> GenerateAllowedList(int max)
+        {
+            List<Vector> places = new List<Vector>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < max; j++)
+                {
+                    places.Add(new Vector(i, j));
+                }
+            }
+            places.Shuffle();
+            return places;
+        }
+
 
         private void CleanUpAll()
         {
@@ -124,6 +202,6 @@ namespace toolcad23.ViewModels
         private BitmapImage GetImage(string cube)
         {
             return new BitmapImage(new Uri(imagePath + cube));
-        }
+        }        
     }
 }
