ce8cd586e47cde1049a9eb125e22ada268dc38e7
Airat <abdrakovairat@gmail.com>
2023-03-07 14:11:13 +0300

    created my own MessageBox that controlled in MessageBoxFactory

---
+++ ViewModels/OtherViewModels/MyMessageBoxViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using toolcad23.ViewModels.Commands;
+
+namespace toolcad23.ViewModels.OtherViewModels
+{
+    internal class MyMessageBoxViewModel : BaseViewModel
+    {
+        private string messageText;
+        public string MessageText
+        {
+            get { return messageText; }
+            set { messageText = value; OnPropertyChanged(); }
+        }
+
+        #region Commands
+        public ICommand CloseWindowCommand { get; set; }
+        #endregion
+
+        internal MyMessageBoxViewModel() : this("") { }
+
+        internal MyMessageBoxViewModel(string msg)
+        {
+            CloseWindowCommand = new DelegateCommand(OnCloseWindowCommand);
+            MessageText = msg;
+        }
+
+        private void OnCloseWindowCommand(object paramenter)
+        {
+            (paramenter as Window).Close();
+        }
+    }
+}
